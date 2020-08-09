40458934bcd7aecc2f09a644a0e4bbf99d20d293
Darrell Yeo <zeniethlily@gmail.com>
2020-08-09 16:01:16 +0800

    profile model and controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  get 'profiles/index'
+  get 'profiles/show'
+  get 'profiles/destroy'
+  get 'profiles/create'
+  get 'profiles/new'
+  get 'profiles/update'
+  get 'profiles/edit'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
