ffd5d031e6c89609035f1043b9e6d0d5e251acd1
Evan-Yeo <me@evanyeo.com>
2020-08-11 11:06:12 +0800

    active storage installed, remember to bundle installl and db:migrate after you pull

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,31 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2020_08_10_094725) do
-
-ActiveRecord::Schema.define(version: 2020_08_10_073022) do
+ActiveRecord::Schema.define(version: 2020_08_11_030015) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "active_storage_attachments", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "record_type", null: false
+    t.bigint "record_id", null: false
+    t.bigint "blob_id", null: false
+    t.datetime "created_at", null: false
+    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
+    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
+  end
+
+  create_table "active_storage_blobs", force: :cascade do |t|
+    t.string "key", null: false
+    t.string "filename", null: false
+    t.string "content_type"
+    t.text "metadata"
+    t.bigint "byte_size", null: false
+    t.string "checksum", null: false
+    t.datetime "created_at", null: false
+    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
 
   create_table "fooditems", force: :cascade do |t|
     t.string "name"
@@ -36,7 +54,6 @@ ActiveRecord::Schema.define(version: 2020_08_10_073022) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["fooditem_id"], name: "index_menus_on_fooditem_id"
   end
-
 
   create_table "orderitems", force: :cascade do |t|
     t.bigint "fooditems_id", null: false
@@ -75,12 +92,9 @@ ActiveRecord::Schema.define(version: 2020_08_10_073022) do
     t.string "phone"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-
     t.string "description"
     t.bigint "user_id", null: false
     t.index ["user_id"], name: "index_profiles_on_user_id"
-
-
   end
 
   create_table "users", force: :cascade do |t|
@@ -97,13 +111,11 @@ ActiveRecord::Schema.define(version: 2020_08_10_073022) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-
-  add_foreign_key "profiles", "users"
-
+  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "menus", "fooditems"
   add_foreign_key "orderitems", "fooditems", column: "fooditems_id"
   add_foreign_key "orders", "users"
   add_foreign_key "payments", "orders"
   add_foreign_key "payments", "users"
-
+  add_foreign_key "profiles", "users"
 end
