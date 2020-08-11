d6daf7a0e8069be811f74bb022249c5f37ec2f9f
zeniethlily <zeniethlily@gmail.com>
2020-08-11 11:25:21 +0800

    fooditems new/show

--- config/routes.rb
+++ config/routes.rb
@@ -53,6 +53,8 @@ Rails.application.routes.draw do
 
   resources :profiles
 
+  resources :fooditems
+
   root 'profiles#index'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
